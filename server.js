30191521acd098f5df22100bc6f26b4bdaa9f508
Klooven <Klooven@users.noreply.github.com>
2018-06-21 13:01:50 +0300

    Cache

--- server.js
+++ server.js
@@ -4,21 +4,31 @@ require("pkginfo")(module, "version");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const memcache = require("memory-cache");
 const db = require("./db");
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+const cache = (req, res, next) => {
+  let cachekey = memcache.get(req.params.redir);
+  if (cachekey) {
+    res.redirect(cachekey.type, cachekey.destination);
+  } else {
+    next();
+  }
+};
 
 app.get("/", (req, res) => {
   res.send(`redirect-server ${module.exports.version} / ${process.env.RS_NAME}`);
 });
 
-app.get("/:redir", (req, res) => {
+app.get("/:redir", cache, (req, res) => {
   db.findOne({path: req.params.redir}, (err, redirect) => {
-    console.log(redirect);
     if (err || !redirect) {
       res.status(404).send("No redirect found");
     } else {
       res.redirect(redirect.type, redirect.destination);
+      memcache.put(redirect.path, redirect);
     }
   });
 });
